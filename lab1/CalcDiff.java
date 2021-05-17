e68df579d0744de6b8c047f1f981b693da52a894
Kaushiki <trivedi.kaushiki@gmail.com>
2021-05-17 23:21:58 +0530

    Added Lab 1 Assignments

---
+++ lab1/CalcDiff.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class CalcDiff {
+
+		public static int calculateDifference(int n) {
+			int ans = 0;
+			int sumSq = 0, sqSum = 0, sum = 0;
+			for (int i = 1; i <= n; i++) {
+				sqSum += i*i;
+				sum += i;
+			}
+			sumSq = (sum*sum);
+			ans = Math.abs(sumSq - sqSum);
+			
+			return ans;
+		}
+		
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter a number: ");
+			int n = sc.nextInt();
+			int ans = calculateDifference(n);
+			System.out.println(ans);
+			
+			sc.close();
+		}
+
+	}
+
---
+++ lab1/CheckNumberEX_6.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+public class CheckNumberEX_6 {
+	
+		static boolean checkNumber(int number) {
+			if (number < 0)
+				number = -number;
+			int last = 10;
+			int curr;
+			
+			while (number > 0) {
+				curr = number%10;
+				if (last < curr)
+					return false;
+				last = curr;
+				number /= 10;
+			}
+			return true;
+		}
+		
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter a number to check increasing or not: ");
+			int n = sc.nextInt();
+			boolean ans = checkNumber(n);
+			if (ans == true)
+				System.out.println(n + " is an increasing number");
+			else
+				System.out.println(n + " is not an increasing number");
+			sc.close();
+		}
+	}
+
